6f143d1a12e02d657a31f7a1ef4f400bd0ebc0e8
Axel <alex070609033@gmail.com>
2025-02-05 19:20:55 +0100

    Episode 6

--- src/main/java/net/bacchus/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/bacchus/tutorialmod/item/ModCreativeModTabs.java
@@ -26,6 +26,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModItems.METAL_DETECTOR.get());
+
                     })
                     .build());
 
