5ae6da3757a2d0a905ffca66ca9073cefde1f861
Axel <alex070609033@gmail.com>
2025-02-06 19:27:45 +0100

    Episode 12 DATAGEN !

---
+++ src/main/java/net/bacchus/tutorialmod/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,52 @@
+package net.bacchus.tutorialmod.datagen;
+
+import net.bacchus.tutorialmod.TutorialMod;
+import net.bacchus.tutorialmod.block.ModBlocks;
+import net.bacchus.tutorialmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, TutorialMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get()).addTag(Tags.Blocks.ORES);
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                        ModBlocks.SOUND_BLOCK.get()
+                );
+
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get(),
+                        ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                        ModBlocks.END_STONE_SAPPHIRE_ORE.get(),
+                        ModBlocks.SOUND_BLOCK.get()
+                );
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL);
+        this.tag(BlockTags.NEEDS_STONE_TOOL);
+        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL);
+        this.tag(Tags.Blocks.NEEDS_GOLD_TOOL);
+
+    }
+}
